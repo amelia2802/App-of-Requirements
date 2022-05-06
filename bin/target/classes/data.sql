79c50eccc039fc7363ab6dd96463e30da7aa0f16
Amelia Dutta <amelia280111dutta@gmail.com>
2022-05-06 21:05:22 +0530

    first commit

---
+++ bin/target/classes/data.sql
@@ -0,0 +1,1 @@
+insert into User (user_fname,user_lname,user_email,user_pass,user_mobile) values ('Peter','Parker','peter.parker@gmail.com','pet_er@9','9871117542');
---
+++ target/classes/data.sql
@@ -0,0 +1,1 @@
+insert into User (user_fname,user_lname,user_email,user_pass,user_mobile) values ('','','','','');
