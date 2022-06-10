db24020e9f692ed4914aed86be1581464d8653be
Amelia Dutta <amelia280111dutta@gmail.com>
2022-06-10 12:23:50 +0530

    enhanced home page

---
+++ target/classes/static/js/home.js
@@ -0,0 +1,29 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sidebar-wrapper").style.width = "30%";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("sidebar-wrapper").style.width = "0";
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
